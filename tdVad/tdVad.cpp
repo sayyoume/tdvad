6e1bfcfcfe6e01eced48a1bf766f69bc8e8fe325
qinxiaowei <qinxiaowei@comisys.net>
2022-02-07 10:10:21 +0800

    更新下

--- tdVad/tdVad.cpp
+++ tdVad/tdVad.cpp
@@ -9,104 +9,106 @@
 #include "simple_vad.h"
 #include "period_format.h"
 #include "file_cut.h"
+#include <map>
 
 using namespace std;
+#define  DEFMODE 4
 
 // 16000 采样率 10ms，  大小 = 160 * 16bits/8 = 320字节 ,
 //
-//int run(FILE* fp, VadInst* vad, struct cut_info* cut);
-//
-//int add_period_activity(struct periods* per, int is_active, int is_last);
-//
-////int add_cut_file_activity(struct cut_info *cut, int is_active, int is_last);
-//SimpleVad	 m_simpVad;
-//PeroidFormat m_peroidFormat;
-//FileCut		 m_fileCut;
-//int main() {
-//
-//	const char filename[] = "D:\\td_work\\tdvad\\pcm\\16k_1.pcm"; // 读取的文件
-//	const char output_filename_prefix[] = "D:\\td_work\\tdvad\\pcm\\16k_111.pcm"; // 保存的文件名
-//	const char output_dir[] = "D:\\td_work\\tdvad\\pcm\\output_pcm"; // 保存的目录
-//	FILE* fp = nullptr;
-//	fopen_s(&fp,filename, "rb");
-//	if (fp == NULL) {
-//		fprintf(stderr, "%s does not exist\n", filename);
-//		return 3;
-//	}
-//	VadInst* vad = m_simpVad.simple_vad_create();
-//	//simple_vad* vad = simple_vad_create();
-//	if (vad == NULL) {
-//		return 4;
-//	}
-//	FILE* fp2 = nullptr;
-//	fopen_s(&fp2,filename, "rb");
-//	struct cut_info* cut = m_fileCut.cut_info_create(fp2);
-//	snprintf(cut->output_filename_prefix, sizeof(cut->output_filename_prefix), "%s",
-//		output_filename_prefix);
-//	snprintf(cut->output_file_dir, sizeof(cut->output_file_dir), "%s",
-//		output_dir);
-//	int res = run(fp, vad, cut);
-//
-//	fclose(fp);
-//	fclose(fp2);
-//	m_simpVad.simple_vad_free(vad);
-//	m_fileCut.cut_info_free(cut);
-//	printf("PROGRAM FINISH\n");
-//	return res;
-//}
-//
-//int run(FILE* fp, VadInst* vad, struct cut_info* cut) {
-//
-//	int16_t data[FRAME_SIZE];
-//	int res = 0;
-//	struct periods* per = m_peroidFormat.periods_create();
-//
-//	while (res == 0) {
-//		res = m_simpVad.read_int16_bytes(fp, data);
-//		if (res <= 1) {
-//			int is_last = (res == 1);
-//			int is_active = m_simpVad.process_vad(vad, data);
-//			add_period_activity(per, is_active, is_last);
-//			int vad_file_res = m_fileCut.cut_add_vad_activity(cut, is_active, is_last);
-//			if (vad_file_res < 0) {
-//				printf("file write success %s\n", cut->result_filename);
-//			}
-//		}
-//		else if (ferror(fp)) {
-//			printf("read failed  ferror result  : %d\n", ferror(fp));
-//		}
-//
-//	}
-//	m_peroidFormat.periods_free(per);
-//
-//	if (res != 1) {
-//		fprintf(stderr, "read file error %d\n", res);
-//		return res;
-//	}
-//	return 0;
-//}
-//
-//
-//int add_period_activity(struct periods* per, int is_active, int is_last) {
-//	static int old_is_active = 0;
-//	static int count = 0;
-//	int res_add = m_peroidFormat.period_add_vad_activity(per, is_active, is_last);
-//	if (res_add != 0) {
-//		return res_add;
-//	}
-//	if (is_active != old_is_active) {
-//		// printf("%s,%d \n", old_is_active ? "A" : "I", count);
-//		// I,1  表示之前的1个FRAME是 INACTIVE的；
-//		// I,1 A,10 表示之前的1个FRAME是 INACTIVE的；第2-10个FRAME是ACTIVE的
-//		// periods_print(per);
-//		old_is_active = is_active;
-//	}
-//	count += 1;
-//	if (is_last) {
-//		m_peroidFormat.periods_print(per);
-//		printf("total frames %d\n", count);
-//	}
-//}
+int run(FILE* fp, VadInst* vad, struct cut_info* cut);
+
+int add_period_activity(struct periods* per, int is_active, int is_last);
+
+//int add_cut_file_activity(struct cut_info *cut, int is_active, int is_last);
+SimpleVad	 m_simpVad;
+PeroidFormat m_peroidFormat;
+FileCut		 m_fileCut;
+int main1() {
+
+	const char filename[] = "D:\\td_work\\tdvad\\pcm\\16k_1.pcm"; // 读取的文件
+	const char output_filename_prefix[] = "D:\\td_work\\tdvad\\pcm\\16k_111.pcm"; // 保存的文件名
+	const char output_dir[] = "D:\\td_work\\tdvad\\pcm\\output_pcm"; // 保存的目录
+	FILE* fp = nullptr;
+	fopen_s(&fp,filename, "rb");
+	if (fp == NULL) {
+		fprintf(stderr, "%s does not exist\n", filename);
+		return 3;
+	}
+	VadInst* vad = m_simpVad.simple_vad_create();
+	//simple_vad* vad = simple_vad_create();
+	if (vad == NULL) {
+		return 4;
+	}
+	FILE* fp2 = nullptr;
+	fopen_s(&fp2,filename, "rb");
+	struct cut_info* cut = m_fileCut.cut_info_create(fp2);
+	snprintf(cut->output_filename_prefix, sizeof(cut->output_filename_prefix), "%s",
+		output_filename_prefix);
+	snprintf(cut->output_file_dir, sizeof(cut->output_file_dir), "%s",
+		output_dir);
+	int res = run(fp, vad, cut);
+
+	fclose(fp);
+	fclose(fp2);
+	m_simpVad.simple_vad_free(vad);
+	m_fileCut.cut_info_free(cut);
+	printf("PROGRAM FINISH\n");
+	return res;
+}
+
+int run(FILE* fp, VadInst* vad, struct cut_info* cut) {
+
+	int16_t data[FRAME_SIZE];
+	int res = 0;
+	struct periods* per = m_peroidFormat.periods_create();
+
+	while (res == 0) {
+		res = m_simpVad.read_int16_bytes(fp, data);
+		if (res <= 1) {
+			int is_last = (res == 1);
+			int is_active = m_simpVad.process_vad(vad, data);
+			add_period_activity(per, is_active, is_last);
+			int vad_file_res = m_fileCut.cut_add_vad_activity(cut, is_active, is_last);
+			if (vad_file_res < 0) {
+				printf("file write success %s\n", cut->result_filename);
+			}
+		}
+		else if (ferror(fp)) {
+			printf("read failed  ferror result  : %d\n", ferror(fp));
+		}
+
+	}
+	m_peroidFormat.periods_free(per);
+
+	if (res != 1) {
+		fprintf(stderr, "read file error %d\n", res);
+		return res;
+	}
+	return 0;
+}
+
+
+int add_period_activity(struct periods* per, int is_active, int is_last) {
+	static int old_is_active = 0;
+	static int count = 0;
+	int res_add = m_peroidFormat.period_add_vad_activity(per, is_active, is_last);
+	if (res_add != 0) {
+		return res_add;
+	}
+	if (is_active != old_is_active) {
+		// printf("%s,%d \n", old_is_active ? "A" : "I", count);
+		// I,1  表示之前的1个FRAME是 INACTIVE的；
+		// I,1 A,10 表示之前的1个FRAME是 INACTIVE的；第2-10个FRAME是ACTIVE的
+		// periods_print(per);
+		old_is_active = is_active;
+	}
+	count += 1;
+	if (is_last) {
+		m_peroidFormat.periods_print(per);
+		printf("total frames %d\n", count);
+	}
+}
 
 typedef struct sAudioData
 {
@@ -116,14 +118,13 @@ typedef struct sAudioData
 
 int main()
 {
-
 	std::vector<SAUDIODATA*> vec;
 
 	VadInst* vadInst = WebRtcVad_Create();
 	WebRtcVad_Init(vadInst);
-	WebRtcVad_set_mode(vadInst, 2);
+	WebRtcVad_set_mode(vadInst, DEFMODE);
 	
-	std::wstring sFileName = L"D:\\td_work\\tdvad\\pcm\\new_mp3.mp3";
+	std::wstring sFileName = L"D:\\pdb\\16k_1.pcm";
 	FILE* fp = nullptr;
 	_wfopen_s(&fp,sFileName.c_str(), L"rb");
 	
@@ -146,21 +147,41 @@ int main()
 			data->iSize = nRead;
 
 			vec.push_back(data);
-
-			
-
 		}
-
 	}
 
-	const char dst[] = "D:\\pdb\\new_mp3_1111111.mp3"; // 读取的文件
+	const char dst[] = "D:\\pdb\\new_mp3_1111111.pcm"; // 读取的文件
 	FILE* fpdst = nullptr;
 	fopen_s(&fpdst, dst, "wb");
 
 
+	//std::map<int, std::vector<SAUDIODATA>> m_map;
 	bool bActive = true;
+	bool bOldActive = true;
+	int ii = 0;
+	std::vector<SAUDIODATA*> vectmp;
 	for (auto it : vec)
 	{
+		
+		if (bActive != bOldActive && bActive == false) {
+
+			char dst[245]{};
+			sprintf_s(dst, "D:\\pdb\\new_1111111_%d.pcm", ++ii);
+			FILE* fdsttemp = nullptr;
+			fopen_s(&fdsttemp, dst, "wb");
+
+			for (auto it : vectmp)
+			{
+				SAUDIODATA* data = it;
+				fwrite(data->buffer, sizeof(char), data->iSize, fdsttemp);
+			}
+			fclose(fdsttemp);
+
+			vectmp.clear();
+		}
+		
+		bOldActive = bActive;
+
 		SAUDIODATA* data = it;
 		short* shortBUff = new short[data->iSize / 2];
 		memset(shortBUff, 0, data->iSize / 2);
@@ -174,17 +195,20 @@ int main()
 		}
 		if (status == 1) { //有语音
 
+			SAUDIODATA* datat = new SAUDIODATA;
+			datat->buffer = new char[data->iSize];
+			memset(datat->buffer, 0, data->iSize);
+			memcpy(datat->buffer, data->buffer, data->iSize);
+			datat->iSize = data->iSize;
 
+			vectmp.push_back(datat);
 			bActive = true;
 			//fwrite(audioFrame, sizeof(short), read_size, fpOutput);
 			//fflush(fpOutput);
 		}
 		if (status == 0) {//静音
-
-
 			bActive = false;
 		}
-
 		fwrite(shortBUff, sizeof(short), data->iSize/2, fpdst);
 	}
 	fclose(fpdst);
